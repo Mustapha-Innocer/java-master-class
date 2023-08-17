3460475fca8277ab6df8885c1026084542d833ec
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-08-17 23:54:13 +0000

    Exercise three solved

---
+++ basics/Exercises/Getting-Started/Exercise3.java
@@ -0,0 +1,16 @@
+public class Exercise3 {
+	public static void main(String[] args) {
+		/*  
+            Write a program that compares two numbers. Use any comparison operator
+            Create 3 variables. 
+            2 variables to store both numbers and 1 variable store the result
+            Print the result 
+        */
+		int number1 = 20;
+		int number2 = 5;
+
+		boolean result = number1 == number2;
+
+		System.out.println(result);
+	}
+}
