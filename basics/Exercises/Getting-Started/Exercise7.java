3506152697237daf5ad613a1213fa1207085e955
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-08-18 00:23:54 +0000

    Exercise seven solved

---
+++ basics/Exercises/Getting-Started/Exercise7.java
@@ -0,0 +1,16 @@
+public class Exercise7 {
+	public static void main(String[] args) {
+		/*  
+            Write a program that compares two numbers. i.e x > y
+            Create 3 variables. 
+            2 to store both numbers and 1 to store the result
+            Print the result 
+        */
+
+		int x = 3;
+		int y = 3;
+		boolean result = x > y;
+
+		System.out.println(result);
+	}
+}
