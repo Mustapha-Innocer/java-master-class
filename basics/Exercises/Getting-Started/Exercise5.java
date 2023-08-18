fedab3680bdd248dc58c9c92537613ddaee8ffa0
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-08-18 00:08:56 +0000

    Exercise five solved

---
+++ basics/Exercises/Getting-Started/Exercise5.java
@@ -0,0 +1,8 @@
+public class Exercise5 {
+	public static void main(String[] args) {
+		// Write a program that transform "Amigoscode" to "AMIGOSCODE"
+		String name = "Amigoscode".toUpperCase();
+
+		System.out.println(name);
+	}
+}
