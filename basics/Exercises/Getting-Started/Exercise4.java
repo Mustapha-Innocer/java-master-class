ad3c51b4bc07902620e2055371e512fc7af98e74
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-08-18 00:05:46 +0000

    Exercise four solved

---
+++ basics/Exercises/Getting-Started/Exercise4.java
@@ -0,0 +1,17 @@
+public class Exercise4 {
+	public static void main(String[] args) {
+		/*  
+            Write a program that compares two String contents.
+            Create 3 variables. 
+            2 to store both strings and 1 to store the result
+            Print the result 
+        */
+
+		String name1 = "John";
+		String name2 = "James";
+
+		boolean result = name1.equals(name2);
+
+		System.out.println(result);
+	}
+}
