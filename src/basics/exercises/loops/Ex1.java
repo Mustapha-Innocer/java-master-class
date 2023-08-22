169e523743d9e44fc1e373f393b6b692c763b46c
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-08-22 22:50:00 +0000

    array filled with 4

--- src/basics/exercises/loops/Ex1.java
+++ src/basics/exercises/loops/Ex1.java
@@ -4,9 +4,15 @@ public class Ex1 {
 	public static void main(String[] args) {
 		// declare an empty integer array of length = 3
     	// print the contents to the console
-
 		int[] numbers = new int[3];
-
 		System.out.println(Arrays.toString(numbers));
+
+		// fill it with the number `4`
+    	// print the array to the console
+		for (int i=0; i<3; i++) {
+			numbers[i] = 4;
+		}
+		System.out.println(Arrays.toString(numbers));
+
 	}
 }
