a05f0510fb2e32880a64926999c5f98d6ea9a674
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-08-22 23:27:04 +0000

    loops: ex9

---
+++ src/basics/exercises/loops/Ex9.java
@@ -0,0 +1,9 @@
+public class Ex9 {
+	public static void main(String[] args) {
+		// create a for loop which goes from 10 down to 0
+    	// print each value
+		for (int i=10; i>=0; i--) {
+			System.out.println(i);
+		}
+	}
+}
